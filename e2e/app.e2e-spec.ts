fcb80877cb9622542abb50c70b588401a9c38cd7
Jason Guo <jiazheng.guo@mail.utoronto.ca>
2018-01-26 00:44:36 -0500

    added end-to-end test cases

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { CodingTestPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
 describe('coding-test App', () => {
   let page: CodingTestPage;
@@ -7,8 +8,43 @@ describe('coding-test App', () => {
     page = new CodingTestPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display list of restaurant-items for valid outcode', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    
+    const outcodeInput = element(by.css('input[name="outcode"]'));
+    const submitButton = element(by.css('button[type="submit"]'));
+    expect(outcodeInput.isPresent()).toBeTruthy();
+    expect(submitButton.isPresent()).toBeTruthy();
+
+    outcodeInput.sendKeys('se20');
+    expect(outcodeInput.getAttribute('value')).toEqual('se20');
+
+    
+    submitButton.click().then(() => {
+      return browser.waitForAngular();
+    }).then(() => {
+      const restaurantItems = element.all(by.css('.restaurant-item'));
+      expect(restaurantItems.count()).toBeGreaterThan(0);
+    });
+  });
+
+  it('should display error message for invalid outcode', () => {
+    page.navigateTo();
+    
+    const outcodeInput = element(by.css('input[name="outcode"]'));
+    const submitButton = element(by.css('button[type="submit"]'));
+    expect(outcodeInput.isPresent()).toBeTruthy();
+    expect(submitButton.isPresent()).toBeTruthy();
+
+    outcodeInput.sendKeys('0');
+    expect(outcodeInput.getAttribute('value')).toEqual('0');
+
+    submitButton.click().then(() => {
+      return browser.waitForAngular();
+    }).then(() => {
+      const errorMessage = element(by.css('.error-message'));
+      expect(errorMessage.isPresent()).toBeTruthy();
+      expect(errorMessage.getText()).not.toEqual('');
+    });
   });
 });
